19034457f8b0650ba42f9ad44f3bfd31bbded7e9
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-03-19 14:58:24 -0300

    feat: component Logo

---
+++ src/Logo.jsx
@@ -0,0 +1,9 @@
+import { ContentLogo } from "./LogoStyle"
+
+const Logo = () => {
+  return(
+    <a href="#"> <ContentLogo src="/logo.png" alt="logo-principal"/></a>
+  )
+}
+
+export default Logo
---
+++ src/LogoStyle.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const ContentLogo = styled.img`
+  width: 110px;
+  cursor: pointer;
+`
