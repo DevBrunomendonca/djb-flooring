88a002a683ca9c405e482ea767cadcb328a7ef5f
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-03-20 08:51:33 -0300

    feat: add component Title

---
+++ src/Title.jsx
@@ -0,0 +1,9 @@
+import { ContentTitle } from "./TitleStyle"
+
+const Title = ( ) => {
+  return(
+    <ContentTitle>Smile-Inducing Floors for Happy Homes</ContentTitle>
+  )
+}
+
+export default Title
---
+++ src/TitleStyle.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const ContentTitle = styled.h1 `
+  color: var(--secondary-color);
+  font-weight: 300;
+  font-size: 6rem;
+  max-width: 800px;
+  text-align: start;
+`
