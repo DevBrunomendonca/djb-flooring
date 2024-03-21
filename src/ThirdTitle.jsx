c5a0fd9d17eec5e7a3e361c004693f3dff110e5f
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-03-21 15:59:43 -0300

    feat: add component ThirdTitle

---
+++ src/ThirdTitle.jsx
@@ -0,0 +1,9 @@
+import { ContentThirdTitle } from "./ThirdTitleStyle"
+
+const ThirdTitle = ({textThirdTitle}) => {
+  return(
+    <ContentThirdTitle>{textThirdTitle}</ContentThirdTitle>
+  )
+} 
+
+export default ThirdTitle
---
+++ src/ThirdTitleStyle.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const ContentThirdTitle = styled.h3`
+
+  font-size: 2.2rem;
+  color: var(--third-color);
+`
