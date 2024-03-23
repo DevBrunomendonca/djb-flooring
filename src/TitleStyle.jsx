a00b872f8cb7b1de0f9685e35bd47cc8f5f5a311
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-03-23 20:20:32 -0300

    update scrollTrigger

--- src/TitleStyle.jsx
+++ src/TitleStyle.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 export const ContentTitle = styled.h1 `
   color: var(--secondary-color);
-  font-weight: 300;
+  font-weight: 400;
   font-size: 6rem;
   max-width: 800px;
   text-align: start;
